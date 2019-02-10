4edd1fdd6e3d90cf10a1cecf9329e329088863e0
ilango04 <47479589+ilango04@users.noreply.github.com>
2019-02-10 19:30:41 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class positive
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n>0)
+System.out.print("Positive");
+else
+System.out.print("Negative");
+}
+}
