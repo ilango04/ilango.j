d059539a3ee7b31078caf5fa825b4d992aed6b41
ilango04 <47479589+ilango04@users.noreply.github.com>
2019-02-11 19:58:07 +0530

    Update vowel.java

--- vowel.java
+++ vowel.java
@@ -7,12 +7,12 @@ Scanner sc=new Scanner(System.in);
 char a=sc.next().charAt(0);
 if((a>='a')&&(a<='z'))
 {
-if((a='a')||(a='e')||(a='i')||(a='o')||(a='u'))
+if((a=='a')||(a=='e')||(a=='i')||(a=='o')||(a=='u'))
 System.out.print("Vowel");
 else
 System.out.print("Consonant");
 }
 else 
-System.out.print('invalid');
+System.out.print("invalid");
 }
 }
