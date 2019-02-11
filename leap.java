6dcb0de7c06d121d33c399d9b746f386bcbe0c25
ilango04 <47479589+ilango04@users.noreply.github.com>
2019-02-11 20:24:13 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class leap
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(n%4==0)
+System.out.print("yes");
+else
+System.out.print("no");
+}
+}
