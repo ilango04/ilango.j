316e442ef72559e2360f37edb41e8af1f587f378
ilango04 <47479589+ilango04@users.noreply.github.com>
2019-02-11 20:27:44 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class hello
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+while(n!=0)
+{
+System.out.println("Hello");
+n--;
+}
+}
+}
