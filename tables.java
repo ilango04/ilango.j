b97b6f777ed85410f8f8ebc4cdec567f4d818bf3
ilango04 <47479589+ilango04@users.noreply.github.com>
2019-02-25 14:36:25 +0530

    Create tables.java

---
+++ tables.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class tables
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=1;i<=n;i++)
+{
+System.out.print(i*n+" ");
+}
+}
+}
