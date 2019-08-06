cbe6554f0943a02532c8e2e861c0498d467a0ddb
Ilango J <47479589+ilango04@users.noreply.github.com>
2019-08-06 15:38:35 +0530

    Add files via upload

---
+++ vertice.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class vertice
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine(),b="",d[]=a.split("\\\\n");
+        int k=sc.nextInt(),v=sc.nextInt();
+        for(int i=0;i<d.length;i++)
+        {
+            int n=0;
+            while(n!=v)
+            {
+                for(int h=0;h<d[i].length();h++)
+                {
+                for(int j=0;j<k;j++)
+                {
+                b+=d[i].charAt(h);
+                }
+                   }
+            b+="\\";
+            b+="n";
+            n++;
+            }
+        }
+        for(int g=0;g<b.length()-2;g++)
+        System.out.print(b.charAt(g));
+    }
+}
